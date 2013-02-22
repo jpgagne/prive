ca9e17ba4808f70cb4f02245c8544f993cf4327c
Patrick <Patrick@LivingRoom>
2013-02-22 00:17:39 -0500

    Contrat E ajoutee

--- src/reclamationsassurance/Contrat.java
+++ src/reclamationsassurance/Contrat.java
@@ -1,18 +1,10 @@
-
 package reclamationsassurance;
 
+public class Contrat {
+    /* A,B,C ou D, auto converti à uppercase sinon exceptionDonneeInvalide //desactive, fait plus haut*/
 
-public class Contrat 
-{
- /* A,B,C ou D, auto converti à uppercase sinon exceptionDonneeInvalide //desactive, fait plus haut*/
-    private char typeContrat;  
-    
-    
-    
+    private char typeContrat;
     //<editor-fold defaultstate="collapsed" desc="attributs du contrat">
-    
-    
-    
     private String soin0Nom = "Massothérapie";
     private int soin0Taux;
     private int soin0Max = -1;
@@ -45,20 +37,20 @@ public class Contrat
     private int soin7Taux;
     private int soin7Max = -1;
     //</editor-fold>
-    
+
     /* constructeur*/
-    Contrat( char typeContrat) throws ExceptionFinProgramme//throws ExceptionDonneeInvalide
+    Contrat(char typeContrat) throws ExceptionFinProgramme//throws ExceptionDonneeInvalide
     {
         this.typeContrat = Character.toUpperCase(typeContrat);
         etablirTaux();
     }
-    
+
     //<editor-fold defaultstate="collapsed" desc="etablirTaux()throws ExceptionDonneeInvalide">
     private void etablirTaux() throws ExceptionFinProgramme//throws ExceptionDonneeInvalide
     {
         
-        switch (typeContrat)
-        {
+        switch (typeContrat) {
+            
             case 'A':
                 soin0Taux = 25;
                 soin0Max = -1;
@@ -76,9 +68,9 @@ public class Contrat
                 soin6Max = -1;
                 soin7Taux = 0;
                 soin7Max = -1;
-                
+
                 break;
-                
+
                 
             case 'B':
                 soin0Taux = 50;
@@ -97,10 +89,10 @@ public class Contrat
                 soin6Max = -1;
                 soin7Taux = 70;
                 soin7Max = -1;
-                
+
                 break;
-                
-                
+
+
             case 'C':
                 soin0Taux = -90;
                 soin0Max = -1;
@@ -118,11 +110,11 @@ public class Contrat
                 soin6Max = -1;
                 soin7Taux = 90;
                 soin7Max = -1;
-                
+
                 break;
+
                 
-            case 'D':
-            {
+            case 'D': {
                 soin0Taux = 100;
                 soin0Max = 85;
                 soin1Taux = 100;
@@ -139,115 +131,117 @@ public class Contrat
                 soin6Max = 100;
                 soin7Taux = 100;
                 soin7Max = 90;
+
+                break;
+            }            
                 
+                
+            case 'E': {
+                soin0Taux = 15;
+                soin0Max = -1;
+                soin1Taux = 25;
+                soin1Max = -1;
+                soin2Taux = 12;
+                soin2Max = -1;
+                soin3Taux = 60;
+                soin3Max = -1;
+                soin4Taux = 25;
+                soin4Max = 15;
+                soin5Taux = 30;
+                soin4Max = 20;
+                soin6Taux = 15;
+                soin6Max = -1;
+                soin7Taux = 22;
+                soin7Max = -1;
+
                 break;
             }
-            default :
-                throw new ExceptionFinProgramme("Contrat type inconnu: "+typeContrat);
-                //throw new ExceptionDonneeInvalide("Contrat type inconnu: "+typeContrat);
+            default:
+                throw new ExceptionFinProgramme("Contrat type inconnu: " + typeContrat);
+            //throw new ExceptionDonneeInvalide("Contrat type inconnu: "+typeContrat);
         }
-    
+
     }
     //</editor-fold>
 
-    
-    
-    
-    
-protected double calculerRemboursement(Integer noSoin, Double dReclamation) throws ExceptionFinProgramme
-{
-     double montant = 0;
-     Integer pourcentageCouvert = 0;
-     Integer seuilMax = 0;
-     
-     
-     System.out.println("Calcul du remboursement: NoSoin:"+noSoin+" demandé:"+dReclamation);
-     switch (noSoin)
-     {
-         case 0:
-         {
-             pourcentageCouvert = this.soin0Taux;
-             seuilMax = this.soin0Max;  
-             break; 
-         }
-        case 100:
-         {
-             pourcentageCouvert = this.soin1Taux;
-             seuilMax = this.soin1Max;  
-             break; 
-         }
-     
-         case 200:
-         {
-             pourcentageCouvert = this.soin2Taux;
-             seuilMax = this.soin2Max;  
-             break; 
-         }
-   
-         case 400:
-         {
-             pourcentageCouvert = this.soin4Taux;
-             seuilMax = this.soin4Max;  
-             break; 
-         }
-        case 500:
-         {
-             pourcentageCouvert = this.soin5Taux;
-             seuilMax = this.soin5Max;  
-             break; 
-         }
-         case 600:
-         {
-             pourcentageCouvert = this.soin6Taux;
-             seuilMax = this.soin6Max;  
-             break; 
-         }
-        case 700:
-         {
-             pourcentageCouvert = this.soin7Taux;
-             seuilMax = this.soin7Max;  
-             break; 
-         }
-         case 800:
-         {
-             others:
-             {
-             if (noSoin < 400 & noSoin >= 300)
-                {
-                pourcentageCouvert = this.soin3Taux;
-                seuilMax = this.soin3Max; 
-                }
-             else 
-                {
-                 throw new ExceptionFinProgramme("No de soin inconnu= "+ noSoin);
-                }
-                
-             }
-             
-         }
-     } //end switch
-     System.out.println("Pourcentage:"+pourcentageCouvert);
-     System.out.println("Max:"+seuilMax);
-   montant = dReclamation*pourcentageCouvert/100;
-   
-             if (seuilMax >= 0)
-             {
-                 if (montant > seuilMax)
-                 {
-                     montant = seuilMax;
-                 }
-                     
-             }
-    System.out.println("Accordé:"+montant);
-    
-  return montant;
+    protected double calculerRemboursement(Integer noSoin, Double dReclamation) throws ExceptionFinProgramme {
+        double montant = 0;
+        Integer pourcentageCouvert = 0;
+        Integer seuilMax = 0;
 
-}
 
-protected char getTypeContrat()
-{
-    return this.typeContrat;
-}
+        System.out.println("Calcul du remboursement: NoSoin:" + noSoin + " demandé:" + dReclamation);
+        switch (noSoin) {
+            case 0: {
+                pourcentageCouvert = this.soin0Taux;
+                seuilMax = this.soin0Max;
+                break;
+            }
+            case 100: {
+                pourcentageCouvert = this.soin1Taux;
+                seuilMax = this.soin1Max;
+                break;
+            }
+
+            case 200: {
+                pourcentageCouvert = this.soin2Taux;
+                seuilMax = this.soin2Max;
+                break;
+            }
+
+            case 400: {
+                pourcentageCouvert = this.soin4Taux;
+                seuilMax = this.soin4Max;
+                break;
+            }
+            case 500: {
+                pourcentageCouvert = this.soin5Taux;
+                seuilMax = this.soin5Max;
+                break;
+            }
+            case 600: {
+                pourcentageCouvert = this.soin6Taux;
+                seuilMax = this.soin6Max;
+                break;
+            }
+            case 700: {
+                pourcentageCouvert = this.soin7Taux;
+                seuilMax = this.soin7Max;
+                break;
+            }
+            case 800: {
+                others:
+                {
+                    if (noSoin < 400 & noSoin >= 300) {
+                        pourcentageCouvert = this.soin3Taux;
+                        seuilMax = this.soin3Max;
+                    } else {
+                        throw new ExceptionFinProgramme("No de soin inconnu= " + noSoin);
+                    }
+
+                }
+
+            }
+        } //end switch
+        System.out.println("Pourcentage:" + pourcentageCouvert);
+        System.out.println("Max:" + seuilMax);
+        montant = dReclamation * pourcentageCouvert / 100;
+
+        if (seuilMax >= 0) {
+            if (montant > seuilMax) {
+                montant = seuilMax;
+            }
+
+        }
+        System.out.println("Accordé:" + montant);
+
+        return montant;
+
+    }
+
+    protected char getTypeContrat() {
+        return this.typeContrat;
+    }
 //protected int getTaux( int noSoin)
 //{
 //    
