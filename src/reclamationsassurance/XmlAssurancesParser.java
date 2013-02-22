7bc4eba6800f704f5e30fb34cf49692e2f084279
patrickng9 <patrickng9@yahoo.ca>
2013-02-22 17:20:44 -0500

    modif mineur

--- src/reclamationsassurance/XmlAssurancesParser.java
+++ src/reclamationsassurance/XmlAssurancesParser.java
@@ -1,14 +1,9 @@
-
 package reclamationsassurance;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,8 +12,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
-
-
 
 
 public class XmlAssurancesParser {
@@ -116,55 +109,52 @@ private void ouvrirFichierSortie(String nomFichierOutput) throws ExceptionFinPro
 
 private void execution() throws ExceptionFinProgramme
 {
-    
     rootElement = docOutput.createElement("remboursements");
     docOutput.appendChild(rootElement);
-    
   
     System.out.println("Element noeud :" + docInput.getDocumentElement().getNodeName());
     
-        // Valider le numéro de client
-        NodeList nodeList = docInput.getElementsByTagName("client");
-        Element elementClient = (Element)nodeList.item(0);
-        String strNoClient = ((Text)elementClient.getFirstChild()).getData();
-        Integer intNoClient = 0;
-        try
-        {
-        intNoClient = Integer.valueOf(strNoClient);
-        }
-        catch (NumberFormatException nfexc)
-        {
-            throw new ExceptionFinProgramme(" Format numero de client invalide: "+ strNoClient);
-        }
-        if ((intNoClient > 999999) || (intNoClient < 100000))
-        {
-             throw new ExceptionFinProgramme(" Numero de client hors bornes: "+ intNoClient);
-        }
-        this.noClient = intNoClient;
-        System.out.println("No Client: "+this.noClient);
+    // Valider le numéro de client
+    NodeList nodeList = docInput.getElementsByTagName("client");
+    Element elementClient = (Element)nodeList.item(0);
+    String strNoClient = ((Text)elementClient.getFirstChild()).getData();
+    Integer intNoClient = 0;
     
+    try {
+        intNoClient = Integer.valueOf(strNoClient);
         
+    } catch (NumberFormatException nfexc) {
+        throw new ExceptionFinProgramme(" Format numero de client invalide: " + strNoClient);
+    }
+    
+    if ((intNoClient > 999999) || (intNoClient < 100000)) {
+        throw new ExceptionFinProgramme(" Numero de client hors bornes: " + intNoClient);
+    }
+    
+    this.noClient = intNoClient;
+    System.out.println("No Client: " + this.noClient);
+       
+         
+    // Valider le type de contrat
+    nodeList = docInput.getElementsByTagName("contrat");
+    Element elementContrat = (Element)nodeList.item(0);
+    String strContrat = ((Text)elementContrat.getFirstChild()).getData();
+    char charTypeContrat;
         
-        
-        // Valider le type de contrat
-        nodeList = docInput.getElementsByTagName("contrat");
-        Element elementContrat = (Element)nodeList.item(0);
-        String strContrat = ((Text)elementContrat.getFirstChild()).getData();
-        char charTypeContrat;
-        if (strContrat.length() != 1)
-            {
-            throw new ExceptionFinProgramme("Type de contrat format invalide");
-            }
-        charTypeContrat = strContrat.charAt(0);
-        charTypeContrat = Character.toUpperCase(charTypeContrat);
-        
-        if (!((charTypeContrat == 'A') || (charTypeContrat == 'B') || (charTypeContrat == 'C') || (charTypeContrat == 'D')))
-         {
-            throw new ExceptionFinProgramme("Type de contrat inconnu: "+charTypeContrat);
-         }   
-            this.typeContrat = charTypeContrat;
-            this.contrat = new Contrat(charTypeContrat);
-            System.out.println("Type de contrat: "+ this.typeContrat);
+    if (strContrat.length() != 1) {
+        throw new ExceptionFinProgramme("Type de contrat format invalide");
+    }
+    
+    charTypeContrat = strContrat.charAt(0);
+    charTypeContrat = Character.toUpperCase(charTypeContrat);
+
+    if (!((charTypeContrat == 'A') || (charTypeContrat == 'B') || (charTypeContrat == 'C') || (charTypeContrat == 'D'))) {
+        throw new ExceptionFinProgramme("Type de contrat inconnu: " + charTypeContrat);
+    }
+    
+    this.typeContrat = charTypeContrat;
+    this.contrat = new Contrat(charTypeContrat);
+    System.out.println("Type de contrat: " + this.typeContrat);
             
      
     NodeList listeNoeudsReclamation = docInput.getElementsByTagName("reclamation");
@@ -253,22 +243,16 @@ private void execution() throws ExceptionFinProgramme
                ecrireRemboursement(nouvelleReclamation);
         }//end if
 
-
     }
-
-
-
+    
 }
     
     
     
-    
-
-    
     private void ecrireRemboursement(Reclamation reclamation) throws ExceptionFinProgramme
     {
         
-       Double montantRembourse = reclamation.getdMontantRemboursable();
+        Double montantRembourse = reclamation.getdMontantRemboursable();
         
         System.out.println("Montant remboursé = "+ montantRembourse);
         
@@ -298,18 +282,18 @@ private void execution() throws ExceptionFinProgramme
 
 private void produireFichier() throws ExceptionFinProgramme
 {
-
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer;
         try {
             transformer = transformerFactory.newTransformer();
-        } catch (TransformerConfigurationException ex) 
-        {
+        } catch (TransformerConfigurationException ex){
             throw new ExceptionFinProgramme(ex.getMessage());
         }
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-		DOMSource source = new DOMSource(docOutput);
-		StreamResult result = new StreamResult(fichierOutput);
+        
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        DOMSource source = new DOMSource(docOutput);
+        StreamResult result = new StreamResult(fichierOutput);
+            
         try {
             // Output to console for testing
             // StreamResult result = new StreamResult(System.out);
@@ -321,7 +305,6 @@ private void produireFichier() throws ExceptionFinProgramme
  
         System.out.println("File saved!");
 }
-
 
 
 }
