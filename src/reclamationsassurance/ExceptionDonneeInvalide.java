642684e0b61b9f4702c4374f4064c385d1592827
patrickng9 <patrickng9@yahoo.ca>
2013-02-22 18:59:21 -0500

    commentaire inutile enlever

--- src/reclamationsassurance/ExceptionDonneeInvalide.java
+++ src/reclamationsassurance/ExceptionDonneeInvalide.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package reclamationsassurance;
 
-/**
- *
- * @author JP 
- * @author PN
- */
 public class ExceptionDonneeInvalide extends Exception {
 
     /**
