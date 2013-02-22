1f16f91ccfb34ad5758d68bfcd54d8f3809625f7
Patrick <Patrick@LivingRoom>
2013-02-21 23:56:25 -0500

    J'ai Alt+Shift+F le TestRead.java

--- src/reclamationsassurance/TestRead.java
+++ src/reclamationsassurance/TestRead.java
@@ -1,39 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package reclamationsassurance;
 
-
-
 public class TestRead {
-  public static void main(String args[]) {
-      System.out.println("Start run");
-      int intNbArgs = args.length;
-      System.out.println("Nb d'Arguments: "+ args.length);
-      
-      try
-      {
-      if (intNbArgs == 2)
-        {
-        XmlAssurancesParser parser = new XmlAssurancesParser( (String) args[0], (String) args[1]);
 
+    public static void main(String args[]) {
+        System.out.println("Start run");
+        int intNbArgs = args.length;
+        System.out.println("Nb d'Arguments: " + args.length);
+
+        try {
+            if (intNbArgs == 2) {
+                XmlAssurancesParser parser = new XmlAssurancesParser((String) args[0], (String) args[1]);
+
+            } else {
+                throw new ExceptionFinProgramme("NB arguments = " + intNbArgs + " / doit être =2");
+            }
+            System.out.println("Exécution terminée avec succès");
+
+        } catch (ExceptionFinProgramme excFP) {
+            System.out.println("ERREUR - L'exécution se termine avec échec");
+            if (excFP.getMessage().length() > 0) {
+                System.out.println(excFP.getMessage() + "/n");
+            }
         }
-      else 
-        {
-            throw new ExceptionFinProgramme("NB arguments = "+intNbArgs+" / doit être =2");
-        }
-       System.out.println("Exécution terminée avec succès");
-       
-      }
-      catch (ExceptionFinProgramme excFP)
-      {
-           System.out.println("ERREUR - L'exécution se termine avec échec");
-        if (excFP.getMessage().length() > 0)
-        {
-            System.out.println(excFP.getMessage()+"/n");
-        }
-      }
-     
-  }
-} 
+    }
+}
