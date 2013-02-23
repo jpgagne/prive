dd026c742fd468cbca3844ebf6cc62641ad455e5
patrickng9 <patrickng9@yahoo.ca>
2013-02-22 19:00:09 -0500

    commentaire inutile enlever

--- src/reclamationsassurance/ReclamationsAssurance.java
+++ src/reclamationsassurance/ReclamationsAssurance.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package reclamationsassurance;
 
-/**
- *
- * @author JP
- */
 public class ReclamationsAssurance {
 
     /**
