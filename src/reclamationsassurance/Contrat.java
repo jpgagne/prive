83e1b48c74f3e43b189813338763dc2c7a8ee952
ed991116 <ed991116@C31239.ens.uqam.ca>
2013-03-07 22:08:08 -0430

    Refactoring

--- src/reclamationsassurance/Contrat.java
+++ src/reclamationsassurance/Contrat.java
@@ -43,7 +43,7 @@ public class Contrat {
     //</editor-fold>
 
     /*
-     * constructeur
+     *  constructeur
      */
     Contrat(char typeContrat) throws ExceptionFinProgramme//throws ExceptionDonneeInvalide
     {
