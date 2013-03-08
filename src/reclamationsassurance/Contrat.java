ddd8b5a7b41d775dacef90a5b34ec84bfa4d9fef
ed991116 <ed991116@C31239.ens.uqam.ca>
2013-03-07 21:54:16 -0430

    refactoring

--- src/reclamationsassurance/Contrat.java
+++ src/reclamationsassurance/Contrat.java
@@ -1,55 +1,50 @@
 package reclamationsassurance;
 
 public class Contrat {
-    /* A,B,C,D ou E, auto converti à uppercase sinon exceptionDonneeInvalide //desactive, fait plus haut*/
+    /*
+     * A,B,C,D ou E, auto converti à uppercase sinon exceptionDonneeInvalide
+     * //desactive, fait plus haut
+     */
 
-    private char typeContrat; 
-    
+    private char typeContrat;
     //<editor-fold defaultstate="collapsed" desc="attributs du contrat">
     private String soin0Nom = "Massothérapie";
     private int soin0Taux;
     private int soin0Max = -1;
-    
     private String soin1Nom = "Ostéopathie";
     private int soin1Taux;
     private int soin1Max = -1;
-    
     // Ajouté
     private String soin150Nom = "Kinésithérapie";
     private int soin150Taux;
     private int soin150Max = -1;
-    
     // Ajouté
     private String soin175Nom = "Médecin généraliste privé";
     private int soin175Taux;
     private int soin175Max = -1;
-    
     private String soin2Nom = "Psychologie individuelle";
     private int soin2Taux;
     private int soin2Max = -1;
-    
     private String soin3Nom = "Soins dentaires";
     private int soin3Taux;
     private int soin3Max = -1;
-    
     private String soin4Nom = "Naturopathie, acupuncture";
     private int soin4Taux;
     private int soin4Max = -1;
-    
     private String soin5Nom = "Chiropratie";
     private int soin5Taux;
     private int soin5Max = -1;
-    
     private String soin6Nom = "Physiothérapie";
     private int soin6Taux;
     private int soin6Max = -1;
-    
     private String soin7Nom = "Ortophonie, ergothérapie";
     private int soin7Taux;
     private int soin7Max = -1;
     //</editor-fold>
 
-    /* constructeur*/
+    /*
+     * constructeur
+     */
     Contrat(char typeContrat) throws ExceptionFinProgramme//throws ExceptionDonneeInvalide
     {
         this.typeContrat = Character.toUpperCase(typeContrat);
@@ -59,71 +54,71 @@ public class Contrat {
     //<editor-fold defaultstate="collapsed" desc="etablirTaux()throws ExceptionDonneeInvalide">
     private void etablirTaux() throws ExceptionFinProgramme//throws ExceptionDonneeInvalide
     {
-        
+
         switch (typeContrat) {
-            
+
             case 'A':
                 soin0Taux = 25;
                 soin0Max = -1;
-                
+
                 soin1Taux = 35;
                 soin1Max = -1;
-                
+
                 soin2Taux = 25;
                 soin2Max = -1;
-                
+
                 soin3Taux = 0;
                 soin3Max = -1;
-                
+
                 soin4Taux = 0;
                 soin4Max = -1;
-                
+
                 soin5Taux = 25;
                 soin5Max = -1;
-                
+
                 soin6Taux = 40;
                 soin6Max = -1;
-                
+
                 soin7Taux = 0;
                 soin7Max = -1;
-                
+
                 soin150Taux = 0;
                 soin150Max = -1;
-                
+
                 soin175Taux = 50;
                 soin175Max = -1;
 
                 break;
 
-                
+
             case 'B':
                 soin0Taux = 50;
                 soin0Max = 40;
-                
+
                 soin1Taux = 50;
                 soin1Max = 50;
-                
+
                 soin2Taux = 100;
                 soin2Max = -1;
-                
+
                 soin3Taux = 50;
                 soin3Max = -1;
-                
+
                 soin4Taux = 0;
                 soin4Max = -1;
-                
+
                 soin5Taux = 50;
                 soin5Max = 50;
-                
+
                 soin6Taux = 100;
                 soin6Max = -1;
-                
+
                 soin7Taux = 70;
                 soin7Max = -1;
-                
+
                 soin150Taux = 0;
                 soin150Max = -1;
-                
+
                 soin175Taux = 75;
                 soin175Max = -1;
 
@@ -133,100 +128,100 @@ public class Contrat {
             case 'C':
                 soin0Taux = 90;
                 soin0Max = -1;
-                
+
                 soin1Taux = 95;
                 soin1Max = -1;
-                
+
                 soin2Taux = 90;
                 soin2Max = -1;
-                
+
                 soin3Taux = 90;
                 soin3Max = -1;
-                
+
                 soin4Taux = 90;
                 soin4Max = -1;
-                
+
                 soin5Taux = 90;
                 soin5Max = -1;
-                
+
                 soin6Taux = 75;
                 soin6Max = -1;
-                
+
                 soin7Taux = 90;
                 soin7Max = -1;
-                
+
                 soin150Taux = 85;
                 soin150Max = -1;
-                
+
                 soin175Taux = 90;
                 soin175Max = -1;
 
                 break;
 
-                
+
             case 'D': {
                 soin0Taux = 100;
                 soin0Max = 85;
-                
+
                 soin1Taux = 100;
                 soin1Max = 75;
-                
+
                 soin2Taux = 100;
                 soin2Max = 100;
-                
+
                 soin3Taux = 100;
                 soin3Max = -1;
-                
+
                 soin4Taux = 100;
                 soin4Max = 65;
-                
+
                 soin5Taux = 100;
                 soin5Max = -1;
-                
+
                 soin6Taux = 100;
                 soin6Max = 100;
-                
+
                 soin7Taux = 100;
                 soin7Max = 90;
-                
+
                 soin150Taux = 100;
                 soin150Max = 150;
-                
+
                 soin175Taux = 95;
                 soin175Max = -1;
 
                 break;
-            }            
-                
-                
+            }
+
+
             case 'E': {
                 soin0Taux = 15;
                 soin0Max = -1;
-                
+
                 soin1Taux = 25;
                 soin1Max = -1;
-                
+
                 soin2Taux = 12;
                 soin2Max = -1;
-                
+
                 soin3Taux = 60;
                 soin3Max = -1;
-                
+
                 soin4Taux = 25;
                 soin4Max = 15;
-                
+
                 soin5Taux = 30;
                 soin5Max = 20;
-                
+
                 soin6Taux = 15;
                 soin6Max = -1;
-                
+
                 soin7Taux = 22;
                 soin7Max = -1;
-                
+
                 soin150Taux = 15;
                 soin150Max = -1;
-                
+
                 soin175Taux = 25;
                 soin175Max = 20;
 
@@ -246,7 +241,7 @@ public class Contrat {
         Integer seuilMax = 0;
 
         System.out.println("Calcul du remboursement: NoSoin:" + noSoin + " demandé:" + dReclamation);
-        
+
         switch (noSoin) {
             case 0: {
                 pourcentageCouvert = this.soin0Taux;
@@ -264,7 +259,7 @@ public class Contrat {
                 seuilMax = this.soin2Max;
                 break;
             }
-                
+
             case 400: {
                 pourcentageCouvert = this.soin4Taux;
                 seuilMax = this.soin4Max;
@@ -294,7 +289,7 @@ public class Contrat {
                 }
             }
         } //end switch
-        
+
         System.out.println("Pourcentage:" + pourcentageCouvert);
         System.out.println("Max:" + seuilMax);
         montant = dReclamation * pourcentageCouvert / 100;
@@ -312,27 +307,5 @@ public class Contrat {
     protected char getTypeContrat() {
         return this.typeContrat;
     }
-//protected int getTaux( int noSoin)
-//{
-//    
-//    
-//    switch(noSoin)
-//    {
-//        case 0:
-//        case 100: 
-//            
-//    }
-//    
-//    
-//    return 0;
-//}
-//protected int getMax( int noSoin)
-//{
-//    return 0;
-//}
-//
-//protected String getNomSoin(int noSoin)
-//{
-//    return "";
-//}
+
 }
