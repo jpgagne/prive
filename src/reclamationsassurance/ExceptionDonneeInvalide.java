49138414d3c95c2e86130692233032eafeb2992b
ck091113 <ck091113@C32046.ens.uqam.ca>
2013-03-11 17:48:48 -0430

    commit recent

--- src/reclamationsassurance/ExceptionDonneeInvalide.java
+++ src/reclamationsassurance/ExceptionDonneeInvalide.java
@@ -2,22 +2,9 @@ package reclamationsassurance;
 
 public class ExceptionDonneeInvalide extends Exception {
 
-    /**
-     * Creates a new instance of
-     * <code>ExceptionDonneeInvalide</code> without detail message.
-     */
-    public ExceptionDonneeInvalide() {
-        System.out.println("Constructeur ExceptionDonneeInvalide");
-        
-    }
-
-    /**
-     * Constructs an instance of
-     * <code>ExceptionDonneeInvalide</code> with the specified detail message.
-     *
-     * @param msg the detail message.
-     */
     public ExceptionDonneeInvalide(String msg) {
         super(msg);
+        System.out.println("Constructeur ExceptionDonneeInvalide");
+        
     }
 }
