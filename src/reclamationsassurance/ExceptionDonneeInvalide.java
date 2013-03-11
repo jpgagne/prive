6339fc5898041fa560b3cea5fb1801e2281e4b12
Jean-Philippe Gagne <jpgagne1@gmail.com>
2013-03-11 19:29:59 -0400

    Refonte des exceptions avec ENUMS - En cours

--- src/reclamationsassurance/ExceptionDonneeInvalide.java
+++ src/reclamationsassurance/ExceptionDonneeInvalide.java
@@ -2,8 +2,22 @@ package reclamationsassurance;
 
 public class ExceptionDonneeInvalide extends Exception {
 
-    public ExceptionDonneeInvalide(String msg) {
+  
+    public ExceptionDonneeInvalide(EnumErreurLecture typeErreur)
+    {
+        super(typeErreur.toString());
+        System.out.println("ExceptionDonneInvalide_ENUM: "+typeErreur.toString());
+    }
+
+      public ExceptionDonneeInvalide(EnumErreurLecture typeErreur, String details)
+    {
+        super(typeErreur.toString());
+        System.out.println("ExceptionDonneInvalide_ENUM+DETAILS: "+typeErreur.toString());
+    }
+    
+    public ExceptionDonneeInvalide(String msg) 
+    {
         super(msg);
-        System.out.println("Constructeur ExceptionDonneeInvalide");
+        System.out.println("ExceptionDonneInvalide_str: "+msg);
     }
 }
