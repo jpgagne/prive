864cbaa91f4c8acdd941418eb5ccdd67a624b9a1
patrickng9 <patrickng9@yahoo.ca>
2013-03-11 17:56:26 -0430

    refactor

--- src/reclamationsassurance/Soin.java
+++ src/reclamationsassurance/Soin.java
@@ -1,52 +1,38 @@
 package reclamationsassurance;
 
-public class Soin 
-{
-private Integer noSoin;    
-private String nom;
-private Integer taux;
-private Integer max;
+public class Soin {
+    private Integer noSoin;
+    private String nom;
+    private Integer taux;
+    private Integer max;
 
+    Soin(Integer noSoin, String nom, Integer taux, Integer max) {
+        this.noSoin = noSoin;
+        this.nom = nom;
+        this.taux = taux;
+        this.max = max;
+    }
 
-Soin(Integer noSoin, String nom, Integer taux, Integer max)
-{
-this.noSoin = noSoin;
-this.nom = nom;
-this.taux = taux;
-this.max = max;
-}
-    
+    Soin(Integer noSoin, String nom, Integer taux) {
+        this.noSoin = noSoin;
+        this.nom = nom;
+        this.taux = taux;
+        this.max = -1;
+    }
 
-Soin(Integer noSoin, String nom, Integer taux)
-{
-this.noSoin = noSoin;
-this.nom = nom;
-this.taux = taux;
-this.max = -1;
-}
+    public Integer getNoSoin() {
+        return noSoin;
+    }
 
-//<editor-fold defaultstate="collapsed" desc="Getters">
-public Integer getNoSoin()
-{
-    return noSoin;
-}
+    public String getNom() {
+        return nom;
+    }
 
-public String getNom()
-{
-    return nom;
-}
+    public Integer getTaux() {
+        return taux;
+    }
 
-public Integer getTaux() {
-    return taux;
-}
-
-public Integer getMax() {
-    return max;
-}
-//</editor-fold>
-
-
-
-
-
+    public Integer getMax() {
+        return max;
+    }
 }
