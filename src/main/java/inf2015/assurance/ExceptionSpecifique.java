ef19da78eedfaeef09dc9109df8d1735937fa5a0
J-P Gagne <jpgagne@gmail.com>
2013-04-18 02:43:56 -0400

    Eceptions sont maintenant plus informatives

--- src/main/java/inf2015/assurance/ExceptionSpecifique.java
+++ src/main/java/inf2015/assurance/ExceptionSpecifique.java
@@ -9,26 +9,37 @@ public ExceptionSpecifique()
     {
     super();
     this.codeErreur = EnumCodeErreur.ERREUR_INCONNUE;
+    afficherException();
     }
 
 public ExceptionSpecifique(EnumCodeErreur codeErreur)
     {
     super();
     this.codeErreur = codeErreur;
+    afficherException();
     }
 
 public ExceptionSpecifique(String message)
     {
     super(message);
     this.codeErreur = EnumCodeErreur.ERREUR_INCONNUE;
+    afficherException();
     }
 
 public ExceptionSpecifique(EnumCodeErreur codeErreur, String message)
     {
     super(message);
     this.codeErreur = codeErreur;
+    afficherException();
     }
 
+
+public void afficherException()
+{
+    System.out.println("EXCEPTION: "+this.getClass().getName()
+            +"  CODE: "+ this.codeErreur
+            +"  MESSAGE: "+super.getMessage());
+}
 
 public EnumCodeErreur getCodeErreur()
     {
